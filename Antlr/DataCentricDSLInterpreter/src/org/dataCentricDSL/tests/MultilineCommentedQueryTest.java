2c0d62a088e12b0b63c0f39b3cee5f5cd1a38399
georgi-ivanov-tues <georgiivanof@abv.bg>
2014-11-26 21:45:31 +0200

    MultilineCommentQueryTest.java added (For Kiro <3)

--- Antlr/DataCentricDSLInterpreter/src/org/dataCentricDSL/tests/MultilineCommentedQueryTest.java
+++ Antlr/DataCentricDSLInterpreter/src/org/dataCentricDSL/tests/MultilineCommentedQueryTest.java
@@ -1,7 +1,7 @@
 package org.dataCentricDSL.tests;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.*;
+
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
@@ -13,18 +13,17 @@ import org.dataCentricDSL.ProgramWalker;
 import org.dataCentricDSL.DataCentricDSLParser.program_return;
 import org.junit.Test;
 
-public class EmptyQueryTest {
+public class MultilineCommentedQueryTest {
 
-	@Test
-	public void EmptyQueryTest() throws RecognitionException {
-		CharStream cs = new ANTLRStringStream("query();");
+	@Test(expected=NullPointerException.class)
+	public void MultilineCommentedQueryTest() throws RecognitionException {
+		CharStream cs = new ANTLRStringStream("/*query();*/");
 		DataCentricDSLLexer lexer = new DataCentricDSLLexer(cs);
 		CommonTokenStream ts = new CommonTokenStream(lexer);
 		DataCentricDSLParser parser = new DataCentricDSLParser(ts);
 		program_return program = parser.program();
 		CommonTreeNodeStream nodeStream = new CommonTreeNodeStream(program.getTree());
 		ProgramWalker walker = new ProgramWalker(nodeStream);
-		String query = walker.program().get(0);
-		assertEquals("asd", query.equals(""));
+		walker.program();
 	}
 }
